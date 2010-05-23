1d8e4e42c2013756081ff095abebfcd5ed64be96
el.iterator <el.iterator@gmail.com>
2010-05-23 03:27:03 +0200

    Added core directory

---
+++ core/Ulteamee/registry/Registry.php
@@ -0,0 +1,132 @@
+<?php  if ( ! defined('BASE_PATH')) exit('No direct script access allowed');
+/*
+ * This file is part of the Ulteamee project package.
+ *
+ * Ulteamee
+ *
+ * An open source Clan Management System for PHP 5.2+ and newer
+ *
+ * @package     Ulteamee
+ * @subpackage  Registry
+ * @author      6dney <6dney@ulteamee-project.org>
+ * @copyright   Copyright (c) 2010 Ulteamee project
+ * @license		http://ulteamee.com/user_guide/license.html
+ * @link		http://ulteamee.com
+ * @since		Version 0.1b
+ *
+*/
+
+
+
+/**
+ * Ulteamee_Registry_Registry
+ *
+ * @author 6dney <6dney@ulteamee-project.org>
+ */
+class Ulteamee_Registry_Registry {
+    /**
+     * Array of variables
+     * @var array
+     */
+    private static $_variables = array();
+
+
+    /**
+     * Instance of the class
+     * @var void
+     */
+    private static $_instance = null;
+
+
+
+
+
+    /**
+     * instantiate this class is not allowed
+     */
+    private function __construct() {
+    }
+
+
+    /**
+     * Cloning is not allowed
+     */
+    private function __clone() {
+    }
+
+
+    /**
+     * Singleton method used to access the object
+     *
+     * @return object Object of the class
+     */
+    public static function getInstance() {
+        if(null === self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+
+    /**
+     * Stores a new variable
+     *
+     * @param string $name The name of the variable
+     * @param mixed $value The value to store
+     */
+    public function set($name, $value) {
+        self::$_instance->_variables[$name] = $value;
+    }
+
+
+    /**
+     * Retrieves a variable's value
+     *
+     * @param string $name The name of the variable
+     * @return mixed
+     */
+    public function get($name) {
+        return (array_key_exists($name, self::$_instance->_variables)) ? self::$_instance->_variables[$name] : null;
+    }
+
+
+    /**
+     * Checks if a variable exists
+     *
+     * @param string $name The name of the variable
+     * @return boolean
+     */
+    public function has($name) {
+        return (array_keys_exists($name));
+    }
+
+
+    /**
+     * Retrieves all stored variables
+     *
+     * @return array
+     */
+    public function get_variables() {
+        return self::$_instance->_variables;
+    }
+
+
+    /**
+     * Deletes a variable
+     *
+     * @param string $name The variable's name to delete
+     */
+    public function delete($name) {
+        unset(self::$_instance->_variables[$name]);
+    }
+
+
+    /**
+     * Erases all stored variables
+     */
+    public function clear() {
+        unset(self::$_instance->_variables);
+        self::$_instance->_variables = array();
+    }
+}
