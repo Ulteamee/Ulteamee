4726e098094a207fcd4997b5879c334e28984d7e
el.iterator <el.iterator@gmail.com>
2010-05-26 02:13:10 +0200

    Deleted the old registry directory

--- web/index.php
+++ web/index.php
@@ -1,6 +1,20 @@
 <?php
+ini_set('error_report', true);
+ini_set('display_errors', true);
+
 define('DS', DIRECTORY_SEPARATOR);
-define('BASE_PATH', realpath(dirname(__FILE__))) .  DS . '..';
+define('BASE_PATH', realpath(dirname(__FILE__) . DS . '..'));
 define('CORE_PATH', BASE_PATH . DS . 'core');
 
+// loader 
+require_once CORE_PATH . DS . 'Ulteamee/Loader/Loader.php';
+Ulteamee_Loader_Loader::register();
+
+require_once CORE_PATH . DS . 'vendors/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$registry = Ulteamee_Registry_Registry::getInstance();
+$registry->set('toto', 2);
+
+print_r($registry->get('toto'));
 ?>
