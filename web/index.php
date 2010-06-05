6b2d97b12542408d3354bbd1f566c0ad81a9783b
el.iterator <el.iterator@gmail.com>
2010-06-05 13:12:17 +0200

    Tiny changes

--- web/index.php
+++ web/index.php
@@ -6,15 +6,19 @@ define('DS', DIRECTORY_SEPARATOR);
 define('BASE_PATH', realpath(dirname(__FILE__) . DS . '..'));
 define('CORE_PATH', BASE_PATH . DS . 'core');
 
-// loader 
+// loader
 require_once CORE_PATH . DS . 'Ulteamee/Loader/Loader.php';
 Ulteamee_Loader_Loader::register();
 
+// dispatcher
+$dispatcher = new Ulteamee_Dispatcher_Dispatcher();
+$dispatcher->init('frontend');
+
+echo '<pre>';
+print_r($dispatcher);
+
+// templater
 require_once CORE_PATH . DS . 'vendors/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$registry = Ulteamee_Registry_Registry::getInstance();
-$registry->set('toto', 2);
-
-print_r($registry->get('toto'));
 ?>
