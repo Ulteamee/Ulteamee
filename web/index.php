fed8092cb403539ae97a7a6cfda28643bddd8ef2
el.iterator <el.iterator@gmail.com>
2010-06-11 00:30:29 +0200

    Added constants APP_PATH & ULTEAMEE_CORE_PATH

--- web/index.php
+++ web/index.php
@@ -4,21 +4,20 @@ ini_set('display_errors', true);
 
 define('DS', DIRECTORY_SEPARATOR);
 define('BASE_PATH', realpath(dirname(__FILE__) . DS . '..'));
+define('APPS_PATH', BASE_PATH . DS . 'apps');
 define('CORE_PATH', BASE_PATH . DS . 'core');
+define('ULTEAMEE_CORE_PATH', CORE_PATH . DS . 'Ulteamee');
 
 // loader
-require_once CORE_PATH . DS . 'Ulteamee/Loader/Loader.php';
-Ulteamee_Loader_Loader::register();
+require_once ULTEAMEE_CORE_PATH . '/Loader/Loader.php';
+Ulteamee_Loader::register();
 
 // dispatcher
-$dispatcher = new Ulteamee_Dispatcher_Dispatcher();
+$dispatcher = new Ulteamee_Dispatcher();
 $dispatcher->init('frontend');
 
-echo '<pre>';
-print_r($dispatcher);
-
 // templater
-require_once CORE_PATH . DS . 'vendors/Twig/Autoloader.php';
+require_once CORE_PATH . '/vendors/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 ?>
